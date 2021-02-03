a08bafd7b4e68f1e11782d9dd318405364734881
lsanson <lazaro.sanson@gmail.com>
2021-02-03 00:07:52 -0300

    adding data annotations to implement model validation

--- Api.Domain/Entities/Product.cs
+++ Api.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.Domain.Entities
 {
@@ -24,11 +25,25 @@ namespace Api.Domain.Entities
 
             Validate();
         }
-
+        [Required]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage="Name is mandatory")]
+        [StringLength(50, MinimumLength=3)]
         public string Name { get; set; }
+        
+        [Required(ErrorMessage="Quantity is mandatory")]
+        [Range(0, int.MaxValue)]
         public int Quantity { get; set; }
-        public decimal UnitValue { get; set; }  
+
+        [Required(ErrorMessage="Unity value is mandatory")]
+        [Range(0.00, double.MaxValue)]
+        [DisplayFormat(DataFormatString="{0:N2")]
+        public decimal UnitValue { get; set; }
+
+        [Required(ErrorMessage="Delivery date is mandatory")]
+        [DisplayFormat(DataFormatString="dd/mm/yyyy")]
+          
         public DateTime DeliveryDate { get; set; }      
         
         #region Methods
